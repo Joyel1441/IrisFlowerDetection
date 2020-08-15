5c840d89271ba17d2920c1e1b5e4f2f23933f04d
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2020-08-15 16:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ data = {"sl":None,"sw":None,"pl":None,"pw":None}
 flower = {"flower":None}
 error= {"type_error":""}
 
-@app.route("/",methods=["POST","GET"])
+@app.route("/")
+def index():
+    data = {"sl":None,"sw":None,"pl":None,"pw":None}
+    flower = {"flower":None}
+    error= {"type_error":""}
+    return redirect(url_for("form"))
+
+@app.route("/form",methods=["POST","GET"])
 def form():
   if request.method == "POST":
     sl = request.form["sl"]
