29080f3c8556ffb9b78f0c598cd811d88298e3cb
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2020-08-15 20:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,22 +12,21 @@ def index():
 def form():
  data = {"sl":None,"sw":None,"pl":None,"pw":None}
  error= {"type_error":""}
- if request.method == "POST":
-    sl = request.form["sl"]
-    sw = request.form["sw"]
-    pl = request.form["pl"]
-    pw = request.form["pw"]
-    try:
-        arr = np.array([[float(sl),float(sw),float(pl),float(pw)]])
-        error["type_error"] = "" 
-    except:
-        error["type_error"] = "ERROR: All values should be number type"
-        return render_template("form.html",error=error["type_error"])
-    classifier = joblib.load("classifier.pkl")
-    pred = classifier.predict(arr)
-    iris_class = {0:"Iris-Setosa",1:"Iris-Versicolour",2:"Iris-Verginica"}
-    flower=iris_class[pred[0]]
-    return render_template("form.html",error=error["type_error"]) 
+ sl = request.form["sl"]
+ sw = request.form["sw"]
+ pl = request.form["pl"]
+ pw = request.form["pw"]
+ try:
+   arr = np.array([[float(sl),float(sw),float(pl),float(pw)]])
+   error["type_error"] = "" 
+ except:
+    error["type_error"] = "ERROR: All values should be number type"
+    return render_template("form.html",error=error["type_error"])
+ classifier = joblib.load("classifier.pkl")
+ pred = classifier.predict(arr)
+ iris_class = {0:"Iris-Setosa",1:"Iris-Versicolour",2:"Iris-Verginica"}
+ flower=iris_class[pred[0]]
+ return render_template("form.html",error=error["type_error"]) 
      
 if __name__ == "__main__":
   app.run(debug = True)
