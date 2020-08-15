ef879848d2e3360b586cef59da4cafc8a1be180a
Joyel1441 <55022719+Joyel1441@users.noreply.github.com>
2020-08-15 19:37:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,52 +3,32 @@ import numpy as np
 import joblib
 
 app = Flask(__name__)
-data = {"sl":None,"sw":None,"pl":None,"pw":None}
-flower = {"flower":None}
-error= {"type_error":""}
 
-@app.route("/",methods=["POST","GET"])
+@app.route("/")
+def index():
+   return render_template("form.html")
+
+@app.route("/form",methods=["POST","GET"])
 def form():
-  if request.method == "POST":
+ data = {"sl":None,"sw":None,"pl":None,"pw":None}
+ error= {"type_error":""}
+ if request.method == "POST":
     sl = request.form["sl"]
     sw = request.form["sw"]
     pl = request.form["pl"]
     pw = request.form["pw"]
-    data["sl"] = sl
-    data["sw"] = sw
-    data["pl"] = pl
-    data["pw"] = pw
-    return redirect(url_for("classify")) 
-  else:
-     return render_template("form.html",flower=flower["flower"],error=error["type_error"])
-
-@app.route("/type")
-def classify():
-      sl = data["sl"]
-      sw = data["sw"]
-      pl = data["pl"]
-      pw = data["pw"]
-      try:
+    try:
         arr = np.array([[float(sl),float(sw),float(pl),float(pw)]])
         error["type_error"] = "" 
       except:
-        data["sl"] = None
-        data["sw"] = None
-        data["pl"] = None
-        data["pw"] = None
-        flower["flower"] = None
         error["type_error"] = "ERROR: All values should be number type"
-        return redirect(url_for("form"))
-      classifier = joblib.load("classifier.pkl")
-      pred = classifier.predict(arr)
-      iris_class = {0:"Iris-Setosa",1:"Iris-Versicolour",2:"Iris-Verginica"}
-      flower["flower"]=iris_class[pred[0]]
-      data["sl"] = None
-      data["sw"] = None
-      data["pl"] = None
-      data["pw"] = None
-      return redirect(url_for("form"))   
-  
+        return render_template("form.html",error=error["type_error"])
+    classifier = joblib.load("classifier.pkl")
+    pred = classifier.predict(arr)
+    iris_class = {0:"Iris-Setosa",1:"Iris-Versicolour",2:"Iris-Verginica"}
+    flower=iris_class[pred[0]]
+    return render_template("form.html",error=error["type_error"]) 
+     
 if __name__ == "__main__":
   app.run(debug = True)
   
